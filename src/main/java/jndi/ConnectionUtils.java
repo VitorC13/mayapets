619448587f8b3d038bbfef560a01dc91ce33b594
vitorcardoso.desenvolvedor@gmail.com <vitorcardoso.desenvolvedor@gmail.com>
2018-11-14 18:07:17 -0200

    Ajust JawsMariaDB

--- src/main/java/jndi/ConnectionUtils.java
+++ src/main/java/jndi/ConnectionUtils.java
@@ -5,7 +5,9 @@ import org.mariadb.jdbc.MySQLDataSource;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.net.URI;
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ConnectionUtils {
 
@@ -14,7 +16,7 @@ public class ConnectionUtils {
         // Create a Connection to database.
         Connection connection = null;
 
-        InitialContext cxt = null;
+        /*InitialContext cxt = null;
 
         cxt  = new InitialContext();
 
@@ -30,8 +32,17 @@ public class ConnectionUtils {
         if (ds == null) {
             throw new Exception("Data source not found!");
 
-        }
-        return connection;
+        }*/
+
+
+        URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+
+        String username = jdbUri.getUserInfo().split(":")[0];
+        String password = jdbUri.getUserInfo().split(":")[1];
+        String port = String.valueOf(jdbUri.getPort());
+        String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+
+        return DriverManager.getConnection(jdbUrl, username, password);
     }
 
     public static void closeQuietly(Connection connection) {
