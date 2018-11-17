01c31ddd02db527cd5f13f5a12d6b04726e7a38e
Vitor Cardoso <vitorcardoso.desenvolvedor@gmail.com>
2018-11-17 14:16:06 -0200

    Update

--- src/main/java/jndi/ConnectionUtils.java
+++ src/main/java/jndi/ConnectionUtils.java
@@ -14,7 +14,7 @@ public class ConnectionUtils {
     public static Connection getConnection() throws NamingException, Exception {
 
         // Create a Connection to database.
-        /*Connection connection = null;
+        Connection connection = null;
 
         InitialContext cxt = null;
 
@@ -32,17 +32,17 @@ public class ConnectionUtils {
         if (ds == null) {
             throw new Exception("Data source not found!");
 
-        }*/
+        }
 
-        URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+        /*URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
 
         String username = jdbUri.getUserInfo().split(":")[0];
         String password = jdbUri.getUserInfo().split(":")[1];
         String port = String.valueOf(jdbUri.getPort());
         String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
 
-        return DriverManager.getConnection(jdbUrl, username, password);
-        //return connection;
+        return DriverManager.getConnection(jdbUrl, username, password);*/
+        return connection;
     }
 
     public static void closeQuietly(Connection connection) {
